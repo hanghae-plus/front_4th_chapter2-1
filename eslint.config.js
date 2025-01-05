1aa34be29e26b90a429f826cc44f67d24892222c
YeongseoYoon <l112xpla@gmail.com>
2025-01-05 21:33:28 +0900

    misc: eslint 설정

---
+++ eslint.config.js
@@ -0,0 +1,76 @@
+import eslint from '@eslint/js';
+import eslintPluginImport from 'eslint-plugin-import';
+import perfectionist from 'eslint-plugin-perfectionist';
+import prettier from 'eslint-plugin-prettier';
+import promise from 'eslint-plugin-promise';
+import security from 'eslint-plugin-security';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import sonarjs from 'eslint-plugin-sonarjs';
+import globals from 'globals';
+
+export default [
+  eslint.configs.recommended,
+  {
+    ignores: ['node_modules/', 'dist/', 'build/', 'eslint.config.js'],
+    languageOptions: {
+      ecmaVersion: 2021,
+      globals: {
+        ...globals.es2021,
+        ...globals.node,
+        ...globals.browser,
+      },
+      sourceType: 'module',
+    },
+    plugins: {
+      import: eslintPluginImport,
+      perfectionist,
+      prettier,
+      promise,
+      security,
+      'simple-import-sort': simpleImportSort,
+      sonarjs,
+    },
+    rules: {
+      // JavaScript
+      curly: ['warn', 'all'],
+      eqeqeq: 'error',
+      // Import
+      'import/newline-after-import': 'warn',
+      'import/no-duplicates': 'warn',
+      'import/order': ['off'],
+      'import/prefer-default-export': 'off',
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+      'no-implicit-coercion': 'error',
+
+      'no-redeclare': 'warn',
+      'no-shadow': 'off',
+      'no-var': 'error',
+      // perfectionist 규칙
+      'perfectionist/sort-objects': [
+        'error',
+        {
+          order: 'asc',
+          type: 'natural',
+        },
+      ],
+
+      'prefer-const': 'warn',
+      // promise 관련 추가 규칙
+      'promise/always-return': 'error',
+
+      'promise/no-return-wrap': 'error',
+
+      'security/detect-non-literal-regexp': 'warn',
+      // security 관련 규칙
+      'security/detect-object-injection': 'warn',
+
+      'simple-import-sort/exports': 'error',
+      // simple-import-sort 규칙
+      'simple-import-sort/imports': 'error',
+
+      // sonarjs 중요 규칙들
+      'sonarjs/cognitive-complexity': ['error', 15],
+      'sonarjs/no-duplicate-string': 'warn',
+    },
+  },
+];
