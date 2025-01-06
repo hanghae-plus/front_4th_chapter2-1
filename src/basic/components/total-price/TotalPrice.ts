b89e64abfa38db8272a514c6a1390f3091f0b0af
Changyeop Song <waitwait0301@naver.com>
2025-01-06 15:16:28 +0900

    fix: 총액 관련 UI, 로직 TotalPrice 파일에 분리

---
+++ src/basic/components/total-price/TotalPrice.ts
@@ -0,0 +1,19 @@
+const TotalPrice = () => {
+  // mock 변수선언
+  const totalAmt = 30000;
+  const discRate = 1000;
+
+  const isShowDiscount = discRate > 0;
+
+  const render = `
+  <div class="text-xl font-bold my-4">
+  총액: ${Math.round(totalAmt)} 원
+  ${
+    isShowDiscount &&
+    `<span class="text-green-500 ml-2">
+  (${(discRate * 100).toFixed(1)}% 할인 적용)
+    </span>`
+  }
+  </div>
+  `;
+};
