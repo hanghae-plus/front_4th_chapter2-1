aedcac4d912e81e4af6566b058e53bb689700043
Changyeop Song <waitwait0301@naver.com>
2025-01-06 15:24:24 +0900

    fix: 카트 아이템 UI, 로직 CartItem 파일에 분리

---
+++ src/basic/components/cart/cart-item/CartItem.ts
@@ -0,0 +1,22 @@
+interface CartItemProps {
+  id: string;
+  name: string;
+  val: number;
+  q: number;
+}
+
+const CartItem = (props) => {
+  const { id, name, val, q } = props;
+
+  const render = `
+    <div id=${id} class="flex justify-between items-center mb-2">
+        <span>
+         ${name} - ${val}원 x 1
+        </span>
+        <div>
+        <button class="quantity-change bg-blue-500 text-white px-2 py-1 rounded mr-1" data-product-id=${id} data-change="1">+</button>
+        <button class="remove-item bg-red-500 text-white px-2 py-1 rounded" data-product-id=${id}>삭제</button>
+        </div>
+    </div>
+  `;
+};
