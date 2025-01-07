0116b9040dcc2008063f604ee4079330fd57cecd
9yurilee <9yurilee@gmail.com>
2025-01-07 17:28:44 +0900

    chore: advanced관련 vite 파일 수정

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      advanced: path.resolve(__dirname, "src/advanced"),
+    },
+  },
   test: {
     globals: true,
     environment: "jsdom",
