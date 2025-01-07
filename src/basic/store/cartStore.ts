38472263a6339f0ade7dfdb4f2fa894ffc1bc456
Changyeop Song <waitwait0301@naver.com>
2025-01-08 00:57:39 +0900

    fix: calcCart 함수 내부에서 총액을 구하는 반복문 내부 카트의 아이템과 매치되는 프로덕트 아이템을 가져오는 로직 getMatchedProductInCart 함수로 분리

--- src/basic/store/cartStore.ts
+++ src/basic/store/cartStore.ts
@@ -8,12 +8,12 @@ export interface Product {
 }
 
 interface CartState {
-  cartList: Product[] | null;
+  cartList: Product[];
   totalPrice: number;
 }
 
 interface CartActions {
-  getCartList: () => Product[] | null;
+  getCartList: () => Product[];
   getCartItem: (id: string) => Product | undefined;
   addCartItem: (item: Product) => void;
   getTotalAmount: () => number;
@@ -22,7 +22,7 @@ interface CartActions {
 
 export const CartStore = createStore<CartState, CartActions>(
   {
-    cartList: null,
+    cartList: [],
     totalPrice: 0,
   },
   (state, notify) => ({
@@ -62,68 +62,61 @@ export const CartStore = createStore<CartState, CartActions>(
 );
 
 // INFO: 카트 계산 로직 함수
-function calcCart() {
+function calcCart(cartList: CartState['cartList']) {
   // 임시 변수
   // let sum;
 
-  // 토탈 가격
-  totalAmt = 0;
-  // 아이템 개수 itemCount
-  itemCnt = 0;
+  const item: Product = {
+    id: 'p1',
+    name: 'dummy',
+    val: 30000,
+    q: 30,
+  };
 
-  // 아이템 객체 (스토어에서 처리하기 때문에 children으로 빼오지 않아도 된다 state에서 빼오면 됨)
-  const cartItems = cartDisp.children;
+  const productList = [item];
+
+  // 토탈 가격
+  let totalAmt = 0;
+  // 아이템 개수 itemCount
+  let itemCnt = 0;
 
   // 뭔지 모르겠음 Total
   let subTot = 0;
 
   // cartCount만큼 반복문
-  for (var i = 0; i < cartItems.length; i++) {
-    (function () {
-      let curItem;
+  for (const cartItem of cartList) {
+    const matchedItem = getMatchedProductInCart(cartItem, productList);
 
-      // 타입 에러 처리를 위한 임시 유효성
-      if (!curItem) return;
+    if (!matchedItem) continue;
 
-      // 카운트 내부에서 프로덕트 리스트 갯수만큼 반복
-      for (let j = 0; j < prodList.length; j++) {
-        // 현재 프로덕트 아이템과 카트 아이템이 같다면
-        if (prodList[j].id === cartItems[i].id) {
-          // 프로덕트 아이템을 currentItem에 할당
-          curItem = prodList[j];
-          break;
-        }
-      }
+    // 재고 (cartStore 내부에서 현재 아이템의 q를 뽑아오면 된다)
+    const matchedItemQuantity = matchedItem.q;
 
-      // 재고 (cartStore 내부에서 현재 아이템의 q를 뽑아오면 된다)
-      const q = parseInt(cartItems[i].querySelector('span').textContent.split('x ')[1]);
+    // 현재 아이템으로 선택된 아이템의 재고 총액
+    const matchedItemTotal = matchedItem.val * matchedItemQuantity;
 
-      // 현재 아이템으로 선택된 녀석들의 총액
-      const itemTot = curItem.val * q;
+    // disc가 뭔지 모르겠지만 변수
+    let disc = 0;
 
-      // disc가 뭔지 모르겠지만 변수
-      let disc = 0;
+    // 재고를 itemCount에 더한다
+    itemCnt += matchedItemQuantity;
 
-      // 재고를 itemCount에 더한다
-      itemCnt += q;
+    // 현재 아이템의 총액을 subTotal에 더한다.
+    subTot += matchedItemTotal;
 
-      // 현재 아이템의 총액을 subTotal에 더한다.
-      subTot += itemTot;
+    // 아이템 갯수별 할인 적용률을 처리하는 로직
+    if (matchedItemQuantity >= 10) {
+      if (matchedItem.id === 'p1') disc = 0.1;
+      else if (matchedItem.id === 'p2') disc = 0.15;
+      else if (matchedItem.id === 'p3') disc = 0.2;
+      else if (matchedItem.id === 'p4') disc = 0.05;
+      else if (matchedItem.id === 'p5') disc = 0.25;
+    }
 
-      // 아이템 갯수별 할인 적용률을 처리하는 로직
-      if (q >= 10) {
-        if (curItem.id === 'p1') disc = 0.1;
-        else if (curItem.id === 'p2') disc = 0.15;
-        else if (curItem.id === 'p3') disc = 0.2;
-        else if (curItem.id === 'p4') disc = 0.05;
-        else if (curItem.id === 'p5') disc = 0.25;
-      }
+    // 총액에 현재 아이템의 총액에 할인율을 계산한 값을 할당
+    totalAmt += matchedItemTotal * (1 - disc);
 
-      // 총액에 현재 아이템의 총액에 할인율을 계산한 값을 할당
-      totalAmt += itemTot * (1 - disc);
-
-      // 반복문 마지막 줄
-    })();
+    // 반복문 마지막 줄
   }
 
   // 할인율 같음
@@ -180,4 +173,12 @@ function calcCart() {
 
   // 보너스 함수 호출
   renderBonusPts();
+}
+
+function getMatchedProductInCart(cartItem: Product, productList: Product[]) {
+  // 아이템 객체 (스토어에서 처리하기 때문에 children으로 빼오지 않아도 된다 state에서 빼오면 됨)
+
+  const matchedProduct = productList.find((product) => product.id === cartItem.id);
+
+  return matchedProduct;
 }
