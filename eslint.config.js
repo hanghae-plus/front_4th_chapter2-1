8db2afec2ad0c606028e6f613698143a36469816
jungeun0712 <kje970712@naver.com>
2025-01-07 22:42:31 +0900

    chore: eslint, prettier 변경 및 추가

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,25 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  { ignores: ['node_modules', 'dist'] },
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  eslintPluginPrettier,
+  eslintConfigPrettier,
+  {
+    rules: {
+      'no-unused-vars': 'warn',
+      'prettier/prettier': [
+        'error',
+        {
+          endOfLine: 'auto', // Ensure endOfLine is set to 'auto'
+        },
+      ],
+    },
+  },
 ];
