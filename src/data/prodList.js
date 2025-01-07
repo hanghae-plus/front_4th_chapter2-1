2393a22e4cd2b26bd282386de78633a58da481b1
JaeHyun Bang <jhbang@plea.kr>
2025-01-08 01:12:20 +0900

    chore : constants 파일 추가
    - 상품 리스트 (PRODUCT_LIST) 선언

---
+++ src/data/prodList.js
@@ -0,0 +1,7 @@
+export const PRODUCT_LIST = [
+  { id: "p1", name: "상품1", cost: 10000, stock: 50 },
+  { id: "p2", name: "상품2", cost: 20000, stock: 30 },
+  { id: "p3", name: "상품3", cost: 30000, stock: 20 },
+  { id: "p4", name: "상품4", cost: 15000, stock: 0 },
+  { id: "p5", name: "상품5", cost: 25000, stock: 10 },
+];
