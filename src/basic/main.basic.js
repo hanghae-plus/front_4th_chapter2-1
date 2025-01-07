abb75d46a6cad7c658c6fcced7af05fcbd12255a
Gyeongmin Kim <gmkim716@gmail.com>
2025-01-07 20:31:38 +0900

    feat: 컨벤션으로 정한 프로젝트 기본 구조에 따라 폴더를 만든다

--- src/basic/main.basic.js
+++ src/basic/main.basic.js
@@ -5,14 +5,6 @@ var lastSel,
   totalAmt = 0,
   itemCnt = 0;
 
-var prodList = [
-  { id: "p1", name: "상품1", val: 10000, q: 50 },
-  { id: "p2", name: "상품2", val: 20000, q: 30 },
-  { id: "p3", name: "상품3", val: 30000, q: 20 },
-  { id: "p4", name: "상품4", val: 15000, q: 0 },
-  { id: "p5", name: "상품5", val: 25000, q: 10 },
-];
-
 function main() {
   var root = document.getElementById("app");
 
@@ -63,6 +55,7 @@ function main() {
 
   calcCart();
 
+  // 행운의 할인 이벤트
   setTimeout(function () {
     setInterval(function () {
       var luckyItem = prodList[Math.floor(Math.random() * prodList.length)];
