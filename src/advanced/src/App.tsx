9664f3107657b0776c67506cda2cfce14352291c
SU_VIN <pyotnqls25@naver.com>
2025-01-09 01:23:30 +0900

    fix: Cart 페이지 초기 세팅

--- src/advanced/src/App.tsx
+++ src/advanced/src/App.tsx
@@ -1,7 +1,12 @@
 import './App.css';
+import Cart from './pages/Cart';
 
 function App() {
-  return <div className="bg-gray-100 min-h-screen w-full p-8"></div>;
+  return (
+    <div className="bg-gray-100 min-h-screen w-full p-8">
+      <Cart />
+    </div>
+  );
 }
 
 export default App;
