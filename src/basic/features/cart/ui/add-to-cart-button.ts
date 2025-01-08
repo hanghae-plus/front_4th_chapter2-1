8052797b6de929aa634dfc48c257a28027e8e532
Geunbaek <rmsqor103@naver.com>
2025-01-09 02:42:43 +0900

    feat: AddToCartButton 이벤트 적용

--- src/basic/features/cart/ui/add-to-cart-button.ts
+++ src/basic/features/cart/ui/add-to-cart-button.ts
@@ -1,9 +1,29 @@
+import { productStore } from "@basic/entities/product";
+import { addEvent } from "@basic/shared/lib/event";
+import { html } from "@basic/shared/lib/render";
+import { cartStore } from "../model";
+
 export function AddToCartButton() {
-  return `    
-    <button 
-      id="add-to-cart"
-      class="bg-blue-500 text-white px-4 py-2 rounded" 
-    >
+  const { products } = productStore.getState();
+  const { addToCart } = cartStore.getState();
+
+  addEvent("add-to-cart", "click", () => {
+    const $productSelect = document.getElementById(
+      "product-select"
+    ) as HTMLSelectElement;
+
+    const productId = $productSelect.value;
+    const selectedProduct = products.find(
+      (product) => product.id === productId
+    );
+
+    if (!selectedProduct) return;
+
+    addToCart(selectedProduct);
+  });
+
+  return html`
+    <button id="add-to-cart" class="bg-blue-500 text-white px-4 py-2 rounded">
       추가
     </button>
   `;
