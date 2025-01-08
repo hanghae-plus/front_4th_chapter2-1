94dfe5af5b2184114612c1c1b9875ac9ad313aad
Changyeop Song <waitwait0301@naver.com>
2025-01-08 23:03:26 +0900

    fix: productList를 분리

--- src/basic/store/productStore.ts
+++ src/basic/store/productStore.ts
@@ -1,4 +1,5 @@
 import { createStore } from '../utils/createStore';
+import { productList } from './constants/productList';
 
 export interface Product {
   id: string;
@@ -20,13 +21,7 @@ interface Actions {
 
 export const ProductStore = createStore<State, Actions>(
   {
-    productList: [
-      { id: 'p1', name: '상품1', val: 10000, q: 50 },
-      { id: 'p2', name: '상품2', val: 20000, q: 30 },
-      { id: 'p3', name: '상품3', val: 30000, q: 20 },
-      { id: 'p4', name: '상품4', val: 15000, q: 0 },
-      { id: 'p5', name: '상품5', val: 25000, q: 10 },
-    ],
+    productList,
   },
   (state, notify) => ({
     getProductList: () => {
