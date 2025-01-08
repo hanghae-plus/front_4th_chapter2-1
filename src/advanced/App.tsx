edf73c3e5fb8d15cd01f5356cda8bcbcdf5110f6
Changyeop Song <waitwait0301@naver.com>
2025-01-09 01:38:40 +0900

    feat: 심화 과제를 위한 리액트 설치 및 세팅

---
+++ src/advanced/App.tsx
@@ -0,0 +1,14 @@
+const App = () => {
+  return (
+    <div className="bg-gray-100 p-8">
+      <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl p-8">
+        {/* Cart */}
+        {/* TotalPrice */}
+        {/* ProductSelector */}
+        <div className="text-sm text-gray-500 mt-2">재고</div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
