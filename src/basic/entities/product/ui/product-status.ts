c04c05a4d672a88b89f0b873013e23a5e138755c
Geunbaek <rmsqor103@naver.com>
2025-01-09 02:46:07 +0900

    feat: ProductStatus 비지니스 로직 적용

--- src/basic/entities/product/ui/product-status.ts
+++ src/basic/entities/product/ui/product-status.ts
@@ -1,7 +1,18 @@
+import { html } from "@basic/shared/lib/render";
+import { productStore } from "../model";
+
 export function ProductStatus() {
-  return `
+  const { products } = productStore.getState();
+
+  return html`
     <div id="stock-status" class="text-sm text-gray-500 mt-2">
-        상품4: 품절
+      ${products
+        .filter((product) => product.q < 5)
+        .map(
+          (product) =>
+            `${product.name}: ${product.q > 0 ? "재고 부족 (" + product.q + "개 남음)" : "품절"}`
+        )
+        .join("\n")}
     </div>
   `;
 }
