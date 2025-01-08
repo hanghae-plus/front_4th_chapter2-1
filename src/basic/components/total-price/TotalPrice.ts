5880222721922f27511f200792b48a404dbe2ebe
Changyeop Song <waitwait0301@naver.com>
2025-01-08 18:03:21 +0900

    fix:  TotalPrice 컴포넌트의 할인율을 Store에서 받도록 처리

--- src/basic/components/total-price/TotalPrice.ts
+++ src/basic/components/total-price/TotalPrice.ts
@@ -4,20 +4,19 @@ export const TotalPrice = () => {
   const { actions } = CartStore;
 
   const totalAmount = actions.getTotalAmount();
+  const totalDiscountRate = actions.getTotalDiscountRate();
 
-  // mock 변수선언
-  const discRate = 1000;
-
-  const isShowDiscount = discRate > 0;
+  const isShowDiscount = totalDiscountRate > 0;
 
   const render = `
   <div id="cart-total" class="text-xl font-bold my-4">
   총액: ${Math.round(totalAmount)} 원
   ${
-    isShowDiscount &&
-    `<span class="text-green-500 ml-2">
-  (${(discRate * 100).toFixed(1)}% 할인 적용)
+    isShowDiscount
+      ? `<span class="text-green-500 ml-2">
+  (${(totalDiscountRate * 100).toFixed(1)}% 할인 적용)
     </span>`
+      : ``
   }
   </div>
   `;
