56e147bbbf41a2920c3f8802e1125ae0bdd03523
Geunbaek <rmsqor103@naver.com>
2025-01-08 15:39:57 +0900

    chore: @types/node 추가 및 alias 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
+import path from "path";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@basic": path.resolve(__dirname, "./src/basic")
+    }
+  },
   test: {
     globals: true,
     environment: "jsdom",
