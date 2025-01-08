c3071f1813d6a1dda0fef67ccd84de0f4cd40256
Changyeop Song <waitwait0301@naver.com>
2025-01-08 23:03:12 +0900

    fix: CartItem의 이벤트 핸들러가 1번만 추가 되도록 처리

--- src/basic/components/cart/cart-item/CartItem.ts
+++ src/basic/components/cart/cart-item/CartItem.ts
@@ -8,6 +8,9 @@ interface CartItemProps {
   q: number;
 }
 
+let isQuantityChangeEventListenerAdded = false;
+let isRemoveCartEventListenerAdded = false;
+
 export const CartItem = (props: CartItemProps) => {
   const { id, name, val } = props;
 
@@ -19,65 +22,74 @@ export const CartItem = (props: CartItemProps) => {
   const cartCount = cartActions.getCartItem(id)?.q;
 
   // INFO: 카트 제거 버튼
-  addEventListener('click', function (event) {
-    // cart-items로 클릭인지 처리
+  if (!isQuantityChangeEventListenerAdded) {
+    addEventListener('click', function (event) {
+      // cart-items로 클릭인지 처리
 
-    const target = (event as MouseEvent).target;
-
-    // 타입 에러 처리를 위한 임시 유효성
-    if (!target) return;
-
-    if (target instanceof HTMLButtonElement && target.classList.contains('quantity-change')) {
-      const prodId = target.dataset.productId;
-
-      if (!prodId) return;
-
-      const itemElem = document.getElementById(prodId);
+      const target = (event as MouseEvent).target;
 
       // 타입 에러 처리를 위한 임시 유효성
-      if (!itemElem || !productList) return;
+      if (!target) return;
 
-      const prod = productList.find(function (p) {
-        return p.id === prodId;
-      });
+      if (target instanceof HTMLButtonElement && target.classList.contains('quantity-change')) {
+        const prodId = target.dataset.productId;
 
-      if (!prod || !cartCount) return;
-      // 카트 아이템 추가
+        if (!prodId) return;
 
-      if (cartCount > 0 && cartCount <= prod.q + cartCount) {
-        productActions.decreaseQ(prod?.id);
-      } else if (cartCount <= 0) {
-        // itemElem.remove();
-        // 삭제하다 넘치면 제거해야함
+        const itemElem = document.getElementById(prodId);
 
-        productActions.decreaseQ(prod?.id);
-      } else {
-        alert('재고가 부족합니다.');
+        // 타입 에러 처리를 위한 임시 유효성
+        if (!itemElem || !productList) return;
+
+        const prod = productList.find(function (p) {
+          return p.id === prodId;
+        });
+
+        if (!prod || !cartCount) return;
+        // 카트 아이템 추가
+
+        if (cartCount > 0 && cartCount <= prod.q + cartCount) {
+          productActions.decreaseQ(prod?.id);
+          cartActions.addCartItem(prod);
+        } else if (cartCount <= 0) {
+          // itemElem.remove();
+          // 삭제하다 넘치면 제거해야함
+
+          productActions.decreaseQ(prod?.id);
+          cartActions.removeCartItem(prod.id);
+        } else {
+          alert('재고가 부족합니다.');
+        }
+
+        // calcCart();
       }
+    });
 
-      // calcCart();
-    }
-  });
+    isQuantityChangeEventListenerAdded = true;
+  }
 
   // INFO: 삭제 버튼
-  addEventListener('click', function (event) {
-    const target = (event as MouseEvent).target;
+  if (!isRemoveCartEventListenerAdded) {
+    addEventListener('click', function (event) {
+      const target = (event as MouseEvent).target;
 
-    if (!productList) return;
+      if (!productList) return;
 
-    if (target instanceof HTMLButtonElement && target.classList.contains('remove-item')) {
-      const productId = target.dataset.productId;
+      if (target instanceof HTMLButtonElement && target.classList.contains('remove-item')) {
+        const productId = target.dataset.productId;
 
-      const product = productList.find(function (product) {
-        return product.id === productId;
-      });
+        const product = productList.find(function (product) {
+          return product.id === productId;
+        });
 
-      if (!product) return;
+        if (!product) return;
 
-      productActions.increaseQ(product.id);
-      cartActions.removeCartItem(id);
-    }
-  });
+        productActions.increaseQ(product.id);
+        cartActions.removeCartItem(id);
+      }
+    });
+    isRemoveCartEventListenerAdded = true;
+  }
 
   const render = `
         <div id=${id} class="flex justify-between items-center mb-2">
@@ -85,9 +97,9 @@ export const CartItem = (props: CartItemProps) => {
              ${name} - ${val}원 x ${cartCount}
             </span>
             <div>
-            <button class="quantity-change bg-blue-500 text-white px-2 py-1 rounded mr-1" data-product-id=${id} data-change="-1">-</button>
-            <button class="quantity-change bg-blue-500 text-white px-2 py-1 rounded mr-1" data-product-id=${id} data-change="1">+</button>
-            <button class="remove-item bg-red-500 text-white px-2 py-1 rounded" data-product-id=${id}>삭제</button>
+            <button class="quantity-change bg-blue-500 text-white px-2 py-1 rounded mr-1" data-product-id="${id}" data-change="-1">-</button>
+            <button class="quantity-change bg-blue-500 text-white px-2 py-1 rounded mr-1" data-product-id="${id}" data-change="1">+</button>
+            <button class="remove-item bg-red-500 text-white px-2 py-1 rounded" data-product-id="${id}">삭제</button>
             </div>
         </div>
       `;
