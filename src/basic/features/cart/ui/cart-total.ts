14e5d2514ec4f93bac7894218313ed99359f10a0
Geunbaek <rmsqor103@naver.com>
2025-01-09 02:45:47 +0900

    feat: CartTotal 비지니스 로직 적용

--- src/basic/features/cart/ui/cart-total.ts
+++ src/basic/features/cart/ui/cart-total.ts
@@ -1,10 +1,25 @@
+import { html } from "@basic/shared/lib/render";
+import { calcDiscountedCost, calcDiscountRate, calcTotalPoint } from "../lib";
+import { cartStore } from "../model";
+
 export function CartTotal() {
-  return `
+  const { cart } = cartStore.getState();
+
+  const discountedCost = calcDiscountedCost(cart);
+  const discountRate = calcDiscountRate(cart);
+  const point = calcTotalPoint(cart);
+
+  return html`
     <div id="cart-total" class="text-xl font-bold my-4">
-        총액: 0원
-        <span id="loyalty-points" class="text-blue-500 ml-2">
-            (포인트: 0)
-        </span>
+      <span>총액: ${discountedCost}원</span>
+      ${discountRate > 0
+        ? html`<span class="text-green-500 ml-2"
+            >(${(discountRate * 100).toFixed(1)}% 할인 적용)</span
+          >`
+        : ""}
+      <span id="loyalty-points" class="text-blue-500 ml-2"
+        >(포인트: ${point})</span
+      >
     </div>
   `;
 }
