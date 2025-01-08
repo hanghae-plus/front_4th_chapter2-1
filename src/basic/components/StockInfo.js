dd365d26e14755cd00331f31140efea932508a0b
anne-hyeyeon <khy2106@naver.com>
2025-01-09 00:32:49 +0900

    fix: 구조 원상복구

--- src/basic/components/StockInfo.js
+++
@@ -1,9 +0,0 @@
-export function renderStockInfo(stockEl, products) {
-  const lowStockProducts = products.filter((item) => item.q < 5);
-  stockEl.textContent = lowStockProducts
-    .map(
-      (item) =>
-        `${item.name}: ${item.q > 0 ? `재고 부족 (${item.q}개 남음)` : "품절"}`,
-    )
-    .join("\n");
-}
--- src/basic/index.js
+++
@@ -1,6 +0,0 @@
-import { initializeApp } from "./app/initializeApp.js";
-import { PRODUCTS } from "./constants/products.js";
-
-document.addEventListener("DOMContentLoaded", () => {
-  initializeApp(PRODUCTS);
-});
