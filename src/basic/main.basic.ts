0bebfb65d44eedf5eb2319ebc80308fb76884f21
JunmanChoi <junman9595@gmail.com>
2025-01-09 21:59:48 +0900

    feat: NewItem 컴포넌트 분리

--- src/basic/main.basic.ts
+++ src/basic/main.basic.ts
@@ -1,4 +1,5 @@
 import { getTotalPriceBeforeSpecialOffer } from './features/cart/actions/getTotalPriceBeforeSpecialOffer';
+import { NewCartItem } from './features/cart/views/NewCartItem';
 import { getStockInfo } from './features/product/actions/getStockInfo';
 import ProductOption from './features/product/views/ProductOption';
 import { productList } from './shared/entity/data/productList';
@@ -151,21 +152,8 @@ AddToCartButton.addEventListener('click', function () {
       const newItem = document.createElement('div');
       newItem.id = itemToAdd.id;
       newItem.className = 'flex justify-between items-center mb-2';
-      newItem.innerHTML =
-        '<span>' +
-        itemToAdd.name +
-        ' - ' +
-        itemToAdd.price +
-        '원 x 1</span><div>' +
-        '<button class="quantity-change bg-blue-500 text-white px-2 py-1 rounded mr-1" data-product-id="' +
-        itemToAdd.id +
-        '" data-change="-1">-</button>' +
-        '<button class="quantity-change bg-blue-500 text-white px-2 py-1 rounded mr-1" data-product-id="' +
-        itemToAdd.id +
-        '" data-change="1">+</button>' +
-        '<button class="remove-item bg-red-500 text-white px-2 py-1 rounded" data-product-id="' +
-        itemToAdd.id +
-        '">삭제</button></div>';
+
+      newItem.innerHTML = NewCartItem({ item: itemToAdd });
       CartItemsView.appendChild(newItem);
       itemToAdd.quantity--;
     }
