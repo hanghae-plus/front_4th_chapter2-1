fab7e9e0aafe0fe13f9607dc1fca81a09b809d57
장원정 <tazoal@tazoal.local>
2025-01-10 02:30:02 +0900

    test: 심화과제 통과를 위한 App 렌더링 테스트 하나 추가

--- src/advanced/__tests__/advanced.test.js
+++ src/advanced/__tests__/advanced.test.js
@@ -1,5 +1,18 @@
-import {describe} from "vitest";
+import { describe } from "vitest";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ProductListContextProvider } from "../contexts";
+import { MainPage } from "../pages";
 
-describe('advanced test', () => {
+describe("advanced test", () => {
+  it("App이 정상적으로 렌더링 되는지 확인", () => {
+    const rootElement = document.createElement("div");
+    rootElement.setAttribute("id", "app");
+    document.body.appendChild(rootElement);
 
+    expect(() => {
+      const root = ReactDOM.createRoot(rootElement);
+      root.render(React.createElement(ProductListContextProvider, null, React.createElement(MainPage)));
+    }).not.toThrow();
+  });
 });
