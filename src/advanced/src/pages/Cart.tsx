0fcd8c4eec683e6c172092c12cd18f3b4d3cff6c
SU_VIN <pyotnqls25@naver.com>
2025-01-09 22:08:19 +0900

    refactor: 컴포넌트 import

--- src/advanced/src/pages/Cart.tsx
+++ src/advanced/src/pages/Cart.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import CartSummary from '../components/cart/CartSummary';
+import ProductSelector from '../components/cart/ProductSelector';
 
-const Cart = () => {
+function Cart() {
   return (
     <div
       id="wrap"
       className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl p-8"
     >
       <h1 className="text-2xl font-bold mb-4">장바구니</h1>
+      <CartSummary />
+      <ProductSelector />
     </div>
   );
-};
+}
 
 export default Cart;
