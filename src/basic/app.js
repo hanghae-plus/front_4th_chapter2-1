4f5ba0b3e7c61c9ed581063329a17afc961593a5
effozen <ashgrayblue0@gmail.com>
2025-01-09 19:06:46 +0900

    feat : App.js 분리 (DOM에 객체 렌더링)

--- src/basic/app.js
+++ src/basic/app.js
@@ -1,9 +1,7 @@
-import { Component } from './libs';
+import { Container } from './components/Container.js';
 
 export function App() {
-  return new Component({
-    name: 'div',
-    props: { id: 'app' },
-    children: [],
-  });
+  const root = document.getElementById('app');
+  // 실제 DOM으로 렌더링해서 붙이는 과정
+  root.appendChild(Container().render());
 }
