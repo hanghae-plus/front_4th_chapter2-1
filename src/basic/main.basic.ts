b4d70bdbcc1eec8df7b94b0b651d21b4ed87b173
JunmanChoi <junman9595@gmail.com>
2025-01-09 22:34:28 +0900

    refactor: luckyAlert 분리

--- src/basic/main.basic.ts
+++ src/basic/main.basic.ts
@@ -1,6 +1,7 @@
 import { getTotalPriceBeforeSpecialOffer } from './features/cart/actions/getTotalPriceBeforeSpecialOffer';
 import { NewCartItem } from './features/cart/views/NewCartItem';
 import { getStockInfo } from './features/product/actions/getStockInfo';
+import { luckyAlert } from './features/product/actions/luckyAlert';
 import ProductOption from './features/product/views/ProductOption';
 import { productList } from './shared/entity/data/productList';
 
@@ -44,33 +45,8 @@ function main() {
   Container.appendChild(Wrapper);
   Root.appendChild(Container);
   calculateCartItems();
-  setTimeout(function () {
-    setInterval(function () {
-      const luckyItem =
-        productList[Math.floor(Math.random() * productList.length)];
-      if (Math.random() < 0.3 && luckyItem.quantity > 0) {
-        luckyItem.price = Math.round(luckyItem.price * 0.8);
-        alert('번개세일! ' + luckyItem.name + '이(가) 20% 할인 중입니다!');
-        updateSelectedOptions();
-      }
-    }, 30000);
-  }, Math.random() * 10000);
-  setTimeout(function () {
-    setInterval(function () {
-      if (lastSelectedItemValue) {
-        const suggest = productList.find(function (item) {
-          return item.id !== lastSelectedItemValue && item.quantity > 0;
-        });
-        if (suggest) {
-          alert(
-            suggest.name + '은(는) 어떠세요? 지금 구매하시면 5% 추가 할인!',
-          );
-          suggest.price = Math.round(suggest.price * 0.95);
-          updateSelectedOptions();
-        }
-      }
-    }, 60000);
-  }, Math.random() * 20000);
+
+  luckyAlert(productList, updateSelectedOptions);
 }
 function updateSelectedOptions() {
   SelectView.innerHTML = '';
