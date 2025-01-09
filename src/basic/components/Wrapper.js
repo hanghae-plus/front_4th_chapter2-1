adf6e9e6556bb1e3df6c6759070fc148e6971115
effozen <ashgrayblue0@gmail.com>
2025-01-10 01:12:08 +0900

    feat : Wraaper.js 내부 렌더링 요소 구현

--- src/basic/components/Wrapper.js
+++ src/basic/components/Wrapper.js
@@ -1,16 +1,23 @@
 import { html } from '../libs/index.js';
 import { Title, CartDisplay, TotalAmount, SelectedProd, AddButton, StockInfo } from './index.js';
-import { updateSelectableOptionsDisplay } from '../utils/index.js';
+import { calculateCart, updateBonusPointsDisplay, updateSelectableOptionsDisplay, updateStockInfoDisplay } from '../utils/index.js';
 import { prodList } from '../store/index.js';
 
 export function Wrapper() {
   const SelectedProdComponent = SelectedProd();
+  const cartDisplayComponent = CartDisplay();
+  const stockInfoComponent = StockInfo();
+  const totalAmountComponent = TotalAmount();
 
-  updateSelectableOptionsDisplay({ component: SelectedProdComponent, items: prodList });
+  const { totalAmount } = calculateCart({ cartDisplayComponent: cartDisplayComponent, prodList });
+
+  updateSelectableOptionsDisplay({ selectedPropComponent: SelectedProdComponent, items: prodList });
+  updateStockInfoDisplay({ StockInfoComponent: stockInfoComponent, prodList });
+  updateBonusPointsDisplay({ totalAmountComponent: totalAmountComponent, totalAmount });
 
   return html`
     <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl p-8">
-      ${Title()} ${CartDisplay()} ${TotalAmount()} ${SelectedProdComponent} ${AddButton()} ${StockInfo()}
+      ${Title()} ${cartDisplayComponent} ${totalAmountComponent} ${SelectedProdComponent} ${AddButton()} ${stockInfoComponent}
     </div>
   `;
 }
