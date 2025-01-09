305e955ce18c0cf86c57f896f41ebaf27b9f74cf
LESANF <nagong1000@naver.com>
2025-01-09 18:41:03 +0900

    refactor: 상품 아이템 옵션 생성 관련 분리 및 이벤트 , 업데이트 구독 처리

--- src/basic/components/Cart/Product/ProductSelector/productSelect.js
+++ src/basic/components/Cart/Product/ProductSelector/productSelect.js
@@ -1,3 +1,4 @@
+import { productStore } from "../../../../store";
 import { combineStyles } from "../../../../utils";
 
 export const ProductSelect = () => {
@@ -8,5 +9,50 @@ export const ProductSelect = () => {
   $productSelect.id = productSelectId;
   $productSelect.className = productSelectStyles;
 
-  return $productSelect;
+  const updateSelectOptions = () => {
+    const products = productStore.get("products");
+    const currentValue = $productSelect.value;
+    const discountInformation = productStore.get("discountInformation");
+
+    // 초기화
+    $productSelect.innerHTML = "";
+
+    // 옵션 생성
+    products.forEach((product) => {
+      const $productOption = document.createElement("option");
+      $productOption.value = product.id;
+
+      const isFlashSaleProduct =
+        discountInformation.isFlashSale &&
+        discountInformation.flashSaleProductId === product.id;
+
+      const isSuggestedProduct =
+        discountInformation.isSuggestedProductId === product.id;
+
+      const price = product.price;
+      const displayPrice = isFlashSaleProduct
+        ? price * 0.8
+        : isSuggestedProduct
+          ? price * 0.95
+          : price;
+
+      $productOption.textContent = `${product.name} - ${displayPrice}원`;
+      $productOption.disabled = product.stock === 0;
+
+      if (product.id === currentValue) $productOption.selected = true;
+
+      $productSelect.appendChild($productOption);
+    });
+  };
+
+  // 이벤트 처리
+  $productSelect.addEventListener("change", (event) => {
+    productStore.set("selectedProductId", event.target.value);
+  });
+
+  // 구독 처리
+  productStore.subscribe("products", updateSelectOptions);
+  productStore.subscribe("discountInformation", updateSelectOptions);
+
+  return { element: $productSelect, initialize: updateSelectOptions };
 };
