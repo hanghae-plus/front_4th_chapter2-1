8548f598eca572b73a8a871834aefbd62e3fe4ac
CodyMan0 <hys83751952@gmail.com>
2025-01-10 07:54:59 +0900

    :sparkles: Vite 번들러 설정

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,27 @@
-import { defineConfig } from 'vitest/config';
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+  plugins: [react()],
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.js',
+    environment: "jsdom",
+    setupFiles: ["./src/setupTests.js"],
+    include: [
+      "src/**/*.{test,spec}.{js,jsx,ts,tsx}",
+      "src/basic/__tests__/**/*.{test,spec}.{js,jsx}",
+      "src/advanced/__tests__/**/*.{test,spec}.{js,jsx,ts,tsx}",
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".ts", ".jsx", ".tsx"],
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        basic: "./index.basic.html",
+        advanced: "./index.advanced.html",
+      },
+    },
   },
 });
