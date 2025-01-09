a267eb190fd951d557942122aa67de2aa8a3ef51
jungeun <jungeun.lee@teamsparta.co>
2025-01-10 00:56:03 +0900

    feat: 재고가 5개 이상이면 재고 부족 알림을 보여주지 않는다.

--- src/advanced/component/LowStockWarning.tsx
+++ src/advanced/component/LowStockWarning.tsx
@@ -3,11 +3,13 @@ import { Product } from '../type/type';
 export const LowStockWarning = ({ products }: { products: Product[] }) => {
   return (
     <div id='stock-status' className='text-sm text-gray-500 mt-2 flex flex-col whitespace-pre-wrap'>
-      {products.map((product) => (
-        <div key={product.id}>
-          {product.name}: {product.quantity > 0 ? `${product.quantity}개 남음` : '품절'}
-        </div>
-      ))}
+      {products
+        .filter((product) => product.quantity <= 5)
+        .map((product) => (
+          <div key={product.id}>
+            {product.name}: {product.quantity > 0 ? `${product.quantity}개 남음` : '품절'}
+          </div>
+        ))}
     </div>
   );
 };
