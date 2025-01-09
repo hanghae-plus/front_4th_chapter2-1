264bfacd28c0870a1f92c0612b4e7f07af4827e8
9yurilee <9yurilee@gmail.com>
2025-01-09 15:44:54 +0900

    refactor: 깜짝 세일, 추천 세일 contextAPI에서 적용

--- src/advanced/contexts/CartContext.tsx
+++ src/advanced/contexts/CartContext.tsx
@@ -1,11 +1,17 @@
-import { useReducer } from "react";
+import { useCallback, useEffect, useReducer } from "react";
 import { Product } from "../models/Product";
-import { INITIAL_QUANTITY } from "advanced/constants";
+import {
+  ALERT_MESSAGES,
+  DISCOUNT_RATES,
+  INITIAL_QUANTITY,
+} from "advanced/constants";
+import { productList } from "advanced/store/productList";
 
 export type CartItem = Product & { quantity: number };
 
 interface CartState {
   items: CartItem[];
+  lastSelectedItem: string | null;
 }
 
 type CartAction =
@@ -19,6 +25,8 @@ const cartReducer = (state: CartState, action: CartAction): CartState => {
       const existingItem = state.items.find(
         (item) => item.id === action.product.id
       );
+
+      state.lastSelectedItem = action.product.id;
 
       if (existingItem) {
         if (existingItem.remaining <= 0) {
@@ -63,6 +71,8 @@ const cartReducer = (state: CartState, action: CartAction): CartState => {
         return state;
       }
 
+      state.lastSelectedItem = action.id;
+
       // 장바구니 잔여 수량 1에서 1감소 시 장바구니에서 삭제
       if (itemToChange.quantity === 1 && action.change === -1) {
         return {
@@ -85,11 +95,14 @@ const cartReducer = (state: CartState, action: CartAction): CartState => {
       };
     }
 
-    case "REMOVE_ITEM":
+    case "REMOVE_ITEM": {
+      state.lastSelectedItem = action.id;
+
       return {
         ...state,
         items: state.items.filter((item) => item.id !== action.id),
       };
+    }
 
     default:
       return state;
@@ -97,7 +110,66 @@ const cartReducer = (state: CartState, action: CartAction): CartState => {
 };
 
 export const useCart = () => {
-  const [cartState, dispatch] = useReducer(cartReducer, { items: [] });
+  const [cartState, dispatch] = useReducer(cartReducer, {
+    items: [],
+    lastSelectedItem: null,
+  });
+
+  // 임의의 시간마다 깜짝세일 20%
+  const setSurpriseSale = useCallback(() => {
+    const luckyItem =
+      productList[Math.floor(Math.random() * productList.length)];
+
+    if (
+      Math.random() < DISCOUNT_RATES.SURPRISE_SALE &&
+      luckyItem.remaining > 0
+    ) {
+      luckyItem.price = Math.round(
+        luckyItem.price * DISCOUNT_RATES.SURPRISE_SALE
+      );
+
+      alert(ALERT_MESSAGES.SURPRISE_SALE(luckyItem.name));
+    }
+  }, []);
+
+  // 추천세일 5%
+  const setRecommendSale = useCallback(() => {
+    if (cartState.lastSelectedItem) {
+      const suggest = productList.find(
+        (item) => item.id !== cartState.lastSelectedItem && item.remaining > 0
+      );
+
+      if (suggest) {
+        alert(ALERT_MESSAGES.RECOMMENDED_SALE(suggest.name));
+
+        suggest.price = Math.round(
+          suggest.price * DISCOUNT_RATES.RECOMMENDED_SALE
+        );
+      }
+    }
+  }, [cartState.lastSelectedItem]);
+
+  useEffect(() => {
+    const createEventSale = (
+      callback: () => void,
+      interval: number,
+      delay: number
+    ) => {
+      const timeoutId = setTimeout(() => {
+        setInterval(callback, interval);
+      }, Math.random() * delay);
+
+      return timeoutId; // 타이머 ID 반환
+    };
+
+    const surpriseTimeout = createEventSale(setSurpriseSale, 30000, 10000);
+    const recommendTimeout = createEventSale(setRecommendSale, 60000, 20000);
+
+    return () => {
+      clearTimeout(surpriseTimeout); // 타임아웃 정리
+      clearInterval(recommendTimeout); // 인터벌 정리
+    };
+  }, [setSurpriseSale, setRecommendSale]);
 
   const addItem = (product: Product) =>
     dispatch({
