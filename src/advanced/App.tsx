05fd461bbcac75cc9e147ed0fb509c73772b6684
Changyeop Song <waitwait0301@naver.com>
2025-01-09 09:52:15 +0900

    feat: TotalPrice 컴포넌트 생성

--- src/advanced/App.tsx
+++ src/advanced/App.tsx
@@ -1,11 +1,12 @@
 import { Cart } from './component/cart/Cart';
+import { TotalPrice } from './component/total-price/TotalPrice';
 
 const App = () => {
   return (
     <div className="bg-gray-100 p-8">
       <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl p-8">
         <Cart />
-        {/* TotalPrice */}
+        <TotalPrice />
         {/* ProductSelector */}
         <div className="text-sm text-gray-500 mt-2">재고</div>
       </div>
---
+++ src/advanced/component/total-price/TotalPrice.tsx
@@ -0,0 +1,20 @@
+// dummy
+const totalAmount = 1000;
+const point = 0;
+const totalDiscountRate = 0.1;
+
+export const TotalPrice = () => {
+  const isShowDiscount = totalDiscountRate > 0;
+
+  return (
+    <div id="cart-total" className="text-xl font-bold my-4">
+      총액: ${Math.round(totalAmount)}원
+      <span id="loyalty-points" className="text-blue-500 ml-2">
+        (포인트: {point})
+      </span>
+      {isShowDiscount && (
+        <span className="text-green-500 ml-2">(${(totalDiscountRate * 100).toFixed(1)}% 할인 적용)</span>
+      )}
+    </div>
+  );
+};
