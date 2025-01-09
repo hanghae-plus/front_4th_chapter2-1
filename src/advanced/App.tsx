ca6c083b8be99a2b24852133159ff0c934fc61b4
Changyeop Song <waitwait0301@naver.com>
2025-01-09 09:48:57 +0900

    feat: 카트 컴포넌트 생성

--- src/advanced/App.tsx
+++ src/advanced/App.tsx
@@ -1,8 +1,10 @@
+import { Cart } from './component/cart/Cart';
+
 const App = () => {
   return (
     <div className="bg-gray-100 p-8">
       <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl p-8">
-        {/* Cart */}
+        <Cart />
         {/* TotalPrice */}
         {/* ProductSelector */}
         <div className="text-sm text-gray-500 mt-2">재고</div>
---
+++ src/advanced/component/cart/Cart.tsx
@@ -0,0 +1,8 @@
+export const Cart = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-4">장바구니</h1>
+      <div id="cart-items">{/* {cartList?.map((item) => CartItem(item).render)} */}</div>
+    </div>
+  );
+};
