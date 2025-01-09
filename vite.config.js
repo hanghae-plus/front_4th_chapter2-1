25b2f3e292859f1836555aad0571bf39be73e7df
SU_VIN <pyotnqls25@naver.com>
2025-01-10 00:18:55 +0900

    fix: 수정

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
+  plugins: [react()],
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/setupTests.js'
-  },
-})
+  }
+});
