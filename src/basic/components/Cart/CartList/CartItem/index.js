889f7519764798bdc1850455e6159838e2421473
LESANF <nagong1000@naver.com>
2025-01-09 20:40:14 +0900

    feat: 상품 관련 이벤트 추가 및 할인, 포인트 표시

--- src/basic/components/Cart/CartList/CartItem/index.js
+++ src/basic/components/Cart/CartList/CartItem/index.js
@@ -1,8 +1,24 @@
+import { changeQuantity, removeProduct } from "../../../../utils";
+
 export const CartItem = () => {
   let $cartItem = document.createElement("div");
   const cartItemId = "cart-items";
 
   $cartItem.id = cartItemId;
+
+  $cartItem.addEventListener("click", (event) => {
+    const $target = event.target;
+    if (!$target.matches("button")) return;
+
+    const productId = $target.dataset.productId;
+
+    if ($target.classList.contains("quantity-change")) {
+      const change = parseInt($target.dataset.change);
+      changeQuantity(productId, change);
+    } else if ($target.classList.contains("remove-item")) {
+      removeProduct(productId);
+    }
+  });
 
   return $cartItem;
 };
