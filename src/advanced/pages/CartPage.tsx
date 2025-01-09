20f38f1ea099ad4fef38634fa2192544b7e0ca60
jch1223 <ico1828@gmail.com>
2025-01-10 01:41:22 +0900

    CartTotal 마이그레이션

--- src/advanced/pages/CartPage.tsx
+++ src/advanced/pages/CartPage.tsx
@@ -1,10 +1,12 @@
 import { CartItems } from '../components/CartItems';
+import { CartTotal } from '../components/CartTotal';
 import { Layout } from '../layout/Layout';
 
 export const CartPage = () => {
   return (
     <Layout title='장바구니'>
       <CartItems />
+      <CartTotal />
     </Layout>
   );
 };
