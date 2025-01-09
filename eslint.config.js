1d7a020aba9d017c2bca4b7810a892b991ce630e
JunmanChoi <junman9595@gmail.com>
2025-01-10 01:10:37 +0900

    chore: eslint - prettier 필요 플러그인 추가

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,8 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -8,4 +10,6 @@ export default [
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  eslintPluginPrettierRecommended,
+  eslintConfigPrettier,
 ];
