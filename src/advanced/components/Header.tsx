3735c2748844669c85c44c715dac2ec69c1f56cb
nogy21 <nogy21@gmail.com>
2025-01-10 01:50:47 +0900

    💄: 헤더 컴포넌트의 글꼴 크기 수정

--- src/advanced/components/Header.tsx
+++ src/advanced/components/Header.tsx
@@ -2,5 +2,5 @@ interface Props {
   title: string;
 }
 export default function Header({ title }: Props) {
-  return <h1 className='text-2l font-bold mb-4'>{title}</h1>;
+  return <h1 className='text-2xl font-bold mb-4'>{title}</h1>;
 }
