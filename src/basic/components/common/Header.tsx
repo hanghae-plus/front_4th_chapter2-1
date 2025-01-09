181d35364e2d5d61f6eb0da5dc7da0a4edd93795
Blue-Kite <bluelemon112@gmail.com>
2025-01-09 12:23:44 +0900

    refactor: Cart 페이지 코드 개선

---
+++ src/basic/components/common/Header.tsx
@@ -0,0 +1,15 @@
+import { createElement } from '@utils/createElement';
+
+const Header = () => {
+  const header = createElement(
+    'h1',
+    {
+      class: 'text-2xl font-bold mb-4',
+    },
+    '장바구니'
+  );
+
+  return header;
+};
+
+export default Header;
--- src/basic/pages/Cart.ts
+++ src/basic/pages/Cart.ts
@@ -1,35 +1,28 @@
 import CartTotal from '@/components/CartTotal';
 import ProductSelect from '@/components/ProductSelect';
 import Stock from '@/components/Stock';
-import { createElement } from '@/utils/createElement';
 
 import AddToCartButton from '@components/AddToCartButton';
 import CartList from '@components/CartList';
+import Header from '@components/common/Header';
 import { additionalSale, randomSale } from '@utils/saleEvent';
 
 const Cart = () => {
   const root = document.getElementById('app');
 
-  const container = createElement('div', {
-    class: 'bg-gray-100 p-8',
-  });
+  const render = () => {
+    root!.innerHTML = `
+    <div class="bg-gray-100 p-8">
+      <div id="wrap" class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl p-8"></div>  
+      </div>
+    `;
 
-  const subContainer = createElement('div', {
-    class: 'max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl p-8',
-  });
+    const wrap = document.getElementById('wrap');
 
-  const header = createElement(
-    'h1',
-    {
-      class: 'text-2xl font-bold mb-4',
-    },
-    '장바구니'
-  );
+    wrap!.append(Header(), CartList(), ProductSelect(), AddToCartButton(), Stock(), CartTotal());
+  };
 
-  subContainer.append(header, CartList(), ProductSelect(), AddToCartButton(), Stock(), CartTotal());
-  container.appendChild(subContainer);
-  root!.appendChild(container);
-
+  render();
   randomSale();
   additionalSale();
 };
