ab0afc112dc9aede92b5a7dec199ec54588720e4
divopsor <divopsor@gmail.com>
2025-01-10 00:45:23 +0900

    refactor: 특가 할인 이벤트 함수 분리

--- src/main.original.js
+++ src/main.original.js
@@ -45,33 +45,39 @@ function main() {
 
   calcCart({ sum, cartDisp, prodList, stockInfo });
 
-  setTimeout(function () {
-    setInterval(function () {
-      var luckyItem = prodList[Math.floor(Math.random() * prodList.length)];
-      if (Math.random() < 0.3 && luckyItem.q > 0) {
-        luckyItem.val = Math.round(luckyItem.val * 0.8);
-        alert('번개세일! ' + luckyItem.name + '이(가) 20% 할인 중입니다!');
-        updateSelOpts({ sel, prodList });
-      }
+  setTimeout(() => {
+    setInterval(() => {
+      executeThunderSale({ sel, prodList });
     }, 30000);
   }, Math.random() * 10000);
 
-  setTimeout(function () {
-    setInterval(function () {
-      if (state.lastSel) {
-        var suggest = prodList.find(function (item) {
-          return item.id != state.lastSel && item.q > 0;
-        });
-        if (suggest) {
-          alert(
-            suggest.name + '은(는) 어떠세요? 지금 구매하시면 5% 추가 할인!',
-          );
-          suggest.val = Math.round(suggest.val * 0.95);
-          updateSelOpts({ sel, prodList });
-        }
-      }
+  setTimeout(() => {
+    setInterval(() => {
+      executeSuggestion({ sel, prodList, state });
     }, 60000);
   }, Math.random() * 20000);
+}
+
+function executeThunderSale({ sel, prodList }) {
+  var luckyItem = prodList[Math.floor(Math.random() * prodList.length)];
+  if (Math.random() < 0.3 && luckyItem.q > 0) {
+    luckyItem.val = Math.round(luckyItem.val * 0.8);
+    alert('번개세일! ' + luckyItem.name + '이(가) 20% 할인 중입니다!');
+    updateSelOpts({ sel, prodList });
+  }
+}
+
+function executeSuggestion({ sel, prodList, state }) {
+  if (state.lastSel) {
+    var suggest = prodList.find(function (item) {
+      return item.id != state.lastSel && item.q > 0;
+    });
+    if (suggest) {
+      alert(suggest.name + '은(는) 어떠세요? 지금 구매하시면 5% 추가 할인!');
+      suggest.val = Math.round(suggest.val * 0.95);
+      updateSelOpts({ sel, prodList });
+    }
+  }
 }
 
 function updateSelOpts({ sel, prodList }) {
