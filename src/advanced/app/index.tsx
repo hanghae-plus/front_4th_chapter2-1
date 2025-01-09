06a33d04dff2bef0270dad53b3db5b1ed885f4ec
Geunbaek <rmsqor103@naver.com>
2025-01-09 14:49:58 +0900

    chore: msw 세팅 및 라이브러리 설치

--- src/advanced/app/index.tsx
+++ src/advanced/app/index.tsx
@@ -1,6 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
+if (process.env.NODE_ENV === "development") {
+  // eslint-disable-next-line import/no-internal-modules
+  const { worker } = await import("../../mocks/browser");
+  await worker.start();
+}
+
 createRoot(document.getElementById("app")!).render(
   <StrictMode>
     <div>asd</div>
