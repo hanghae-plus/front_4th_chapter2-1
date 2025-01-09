ccda04cfae41c3d134ff0be0ffb5188b64dec5da
jmlee9707 <jmlee9707@naver.com>
2025-01-09 20:57:20 +0900

    ⬆️ setup : react, typescript 세팅

---
+++ src/advanced/App.tsx
@@ -0,0 +1,10 @@
+const App = () => {
+	return (
+		<div id="app">
+			<div className="bg-gray-100 p-8">
+				<div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl p-8"></div>
+			</div>
+		</div>
+	);
+};
+export default App;
