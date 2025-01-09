848d28c126c39dfafbf07e9c3b229d4fe9d1591e
pitang <wpeffect@gmail.com>
2025-01-10 06:02:39 +0900

    ✅ 테스트 코드 확장자 수정 및 패스
    - test 코드 파일 확장자 변경

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     globals: true,
+    include: ['**/*.test.{js,ts,jsx,tsx}'],
     environment: 'jsdom',
-    setupFiles: './src/setupTests.js'
+    setupFiles: './src/setupTests.js',
   },
-})
+});
