af6911a4aa8f7d28ccabc48cec3cc7be07b7f1e7
LESANF <nagong1000@naver.com>
2025-01-09 18:41:23 +0900

    feat: cart관련 상수화 분리

---
+++ src/basic/constants/cart/index.js
@@ -0,0 +1,9 @@
+export const QUANTITY_DISCOUNT_RATES = {
+  p1: 0.1, // 10% 할인
+  p2: 0.15, // 15% 할인
+  p3: 0.2, // 20% 할인
+  p4: 0.05, // 5% 할인
+  p5: 0.25, // 25% 할인
+};
+
+export const SPECIAL_DISCOUNT_DAY = 2;
