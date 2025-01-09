bfcece1c71a30456609cc894792ccbfb0facd92a
jungeun0712 <kje970712@naver.com>
2025-01-10 02:42:00 +0900

    chore: vite config 변경

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.js'
-  },
+  plugins: [react()],
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'] // 파일 확장자 지원
+  }
 })
