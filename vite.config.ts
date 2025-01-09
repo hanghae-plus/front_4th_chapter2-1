3d2cf057612e46a04023531d8f62054d0b084604
Geunbaek <rmsqor103@naver.com>
2025-01-09 14:13:50 +0900

    chore: advanced 환경설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import path from "path";
+import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+  plugins: [react()],
   resolve: {
     alias: {
-      "@basic": path.resolve(__dirname, "./src/basic")
+      "@basic": path.resolve(__dirname, "./src/basic"),
+      "@advanced": path.resolve(__dirname, "./src/advanced")
     }
   },
   test: {
