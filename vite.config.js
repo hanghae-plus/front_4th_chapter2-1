5a2189c0438d105cb076c3a3a82e68bc1267f9c7
LESANF <nagong1000@naver.com>
2025-01-10 00:16:30 +0900

    chore: react, ts관련 테스트 환경 라이브러리 설치

--- vite.config.js
+++ vite.config.js
@@ -8,11 +8,9 @@ export default defineConfig({
     environment: "jsdom",
     setupFiles: "./src/setupTests.js",
   },
-  // TypeScript와 React 파일 지원
   resolve: {
     extensions: [".js", ".ts", ".jsx", ".tsx"],
   },
-  // 필요한 경우 별도의 빌드 설정
   build: {
     rollupOptions: {
       input: {
