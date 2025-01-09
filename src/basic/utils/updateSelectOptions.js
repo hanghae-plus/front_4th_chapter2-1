fb7e2f363afe58fb29c908d6af96ed5d5d03df70
effozen <ashgrayblue0@gmail.com>
2025-01-09 19:05:27 +0900

    refactor : updateSelectOptions 가독성 개선

--- src/basic/utils/updateSelectOptions.js
+++ src/basic/utils/updateSelectOptions.js
@@ -2,10 +2,15 @@ import { html } from '../libs/index.js';
 
 export const updateSelectOptions = ({ component, items }) => {
   component.update({ children: [''] });
+
   items.forEach(item => {
-    const opt = html`<option value=${item.id} disabled=${item.qty === 0}>
+    const optComponent = html`<option
+      value=${item.id}
+      disabled=${item.qty === 0}
+    >
       ${item.name} - ${item.val}원
     </option>`;
-    component.update({ children: [...component.children, opt] });
+
+    component.update({ children: [...component.children, optComponent] });
   });
 };
