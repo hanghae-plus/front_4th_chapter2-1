b8238f2405b88a9733a23e61f11a3c851043614e
LESANF <nagong1000@naver.com>
2025-01-09 23:46:43 +0900

    fix: prettier 충돌 관련 패키지 설치 및 수정

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,10 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
-import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
 import tseslint from "@typescript-eslint/eslint-plugin";
 import tsParser from "@typescript-eslint/parser";
+import reactPlugin from "eslint-plugin-react";
+import reactHooksPlugin from "eslint-plugin-react-hooks";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -23,13 +24,32 @@ export default [
         ...globals.node,
         ...globals.es2021,
       },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+        ecmaVersion: 2021,
+      },
     },
   },
   pluginJs.configs.recommended,
   {
     files: ["**/*.{js,jsx}"],
+    plugins: {
+      react: reactPlugin,
+      "react-hooks": reactHooksPlugin,
+    },
     rules: {
       "no-unused-vars": "off",
+      "react/react-in-jsx-scope": "off",
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+      "prettier/prettier": "off",
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
     },
   },
   {
@@ -42,13 +62,25 @@ export default [
     },
     plugins: {
       "@typescript-eslint": tseslint,
+      react: reactPlugin,
+      "react-hooks": reactHooksPlugin,
     },
     rules: {
       ...tseslint.configs.recommended.rules,
       "no-unused-vars": "off",
       "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "react/react-in-jsx-scope": "off",
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+      "prettier/prettier": "off",
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
     },
   },
-  eslintPluginPrettier,
   eslintConfigPrettier,
 ];
