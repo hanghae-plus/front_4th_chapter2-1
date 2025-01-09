2bdef883c202c74d39baba02e305f7e837eb27df
YeongseoYoon <l112xpla@gmail.com>
2025-01-09 22:42:15 +0900

    refactor: ProductSelect 컴포넌트를 .tsx로 변환

---
+++ src/advanced/components/ProductSelect.tsx
@@ -0,0 +1,15 @@
+import { ELEMENT_IDS } from '../constants/element-id';
+
+const ProductSelect = ({ products }) => {
+  return (
+    <select id={ELEMENT_IDS.PRODUCT_SELECT} className="border rounded p-2 mr-2">
+      {products.map(({ id, name, price, quantity }) => (
+        <option key={id} value={id} disabled={quantity === 0}>
+          {name} - {price}원
+        </option>
+      ))}
+    </select>
+  );
+};
+
+export default ProductSelect;
