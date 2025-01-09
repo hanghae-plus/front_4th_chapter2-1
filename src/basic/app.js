0a069015f64e4ce63412c04f7eb8029a6dae2ae2
effozen <ashgrayblue0@gmail.com>
2025-01-10 04:13:27 +0900

    refactor : app.js 파일명 수정

--- src/basic/app.js
+++ src/basic/app.js
@@ -2,7 +2,7 @@ import { Container } from './components/Container.js';
 
 export function App() {
   const root = document.getElementById('app');
-  // 실제 DOM으로 렌더링해서 붙이는 과정
 
+  // 실제 DOM으로 렌더링해서 붙이는 과정
   root.appendChild(Container().render());
 }
--- src/basic/main.basic.js
+++ src/basic/main.basic.js
@@ -1,4 +1,4 @@
-import { App } from './App.js';
+import { App } from './app.js';
 
 function main() {
   App();
