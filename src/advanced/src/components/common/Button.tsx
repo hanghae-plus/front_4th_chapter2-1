e89d86ccb2104ccb2b5f5686eaafb217b00d6084
effozen <ashgrayblue0@gmail.com>
2025-01-10 10:19:44 +0900

    feat : Button 및 CartItem 컴포넌트 수정

--- src/advanced/src/components/common/Button.tsx
+++ src/advanced/src/components/common/Button.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
-export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  children: React.ReactNode;
+  className?: string;
+}
 
-export function Button({ children, ...props }: ButtonProps) {
-  return <button {...props}>{children}</button>;
+export function Button({ children, className, ...props }: ButtonProps) {
+  return (
+    <button className={`px-4 py-2 rounded ${className}`} {...props}>
+      {children}
+    </button>
+  );
 }
