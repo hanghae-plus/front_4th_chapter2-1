a3d5a3fd115afb0d5bf2c18170556b9c430c3ba3
JaeHyun Bang <jhbang@plea.kr>
2025-01-10 09:03:01 +0900

    refactor : cartDisp 클릭 이벤트 수정

--- src/basic/main.basic.js
+++ src/basic/main.basic.js
@@ -146,45 +146,12 @@ addBtn.addEventListener("click", function () {
 });
 
 cartDisp.addEventListener("click", function (event) {
-  var tgt = event.target;
+  var target = event.target;
   if (
-    tgt.classList.contains("quantity-change") ||
-    tgt.classList.contains("remove-item")
+    target.classList.contains("quantity-change") ||
+    target.classList.contains("remove-item")
   ) {
-    var prodId = tgt.dataset.productId;
-    var itemElem = document.getElementById(prodId);
-    var prod = PRODUCT_LIST.find(function (p) {
-      return p.id === prodId;
-    });
-    if (tgt.classList.contains("quantity-change")) {
-      var qtyChange = parseInt(tgt.dataset.change);
-      var newQty =
-        parseInt(itemElem.querySelector("span").textContent.split("x ")[1]) +
-        qtyChange;
-      if (
-        newQty > 0 &&
-        newQty <=
-          prod.stock +
-            parseInt(itemElem.querySelector("span").textContent.split("x ")[1])
-      ) {
-        itemElem.querySelector("span").textContent =
-          itemElem.querySelector("span").textContent.split("x ")[0] +
-          "x " +
-          newQty;
-        prod.stock -= qtyChange;
-      } else if (newQty <= 0) {
-        itemElem.remove();
-        prod.stock -= qtyChange;
-      } else {
-        alert("재고가 부족합니다.");
-      }
-    } else if (tgt.classList.contains("remove-item")) {
-      var remQty = parseInt(
-        itemElem.querySelector("span").textContent.split("x ")[1]
-      );
-      prod.stock += remQty;
-      itemElem.remove();
-    }
+    handleDeleteToCart(target);
     calcCart();
   }
 });
