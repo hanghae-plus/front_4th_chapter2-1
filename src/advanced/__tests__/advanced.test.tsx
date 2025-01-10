e000182d34d99f73c63e9096199b57d5c683e2ce
JaeHyun Bang <jhbang@plea.kr>
2025-01-10 10:37:31 +0900

    chore : ts 대응

---
+++ src/advanced/__tests__/advanced.test.tsx
@@ -0,0 +1,12 @@
+import React from "react"; // React import 추가
+import { render, screen } from "@testing-library/react";
+import { describe, it, expect } from "vitest";
+import { App } from "../src/App";
+
+describe("advanced test", () => {
+  it("초기 상태: 상품 목록이 올바르게 렌더링 되는지 확인", () => {
+    render(<App />);
+    const select = screen.getByRole("combobox");
+    expect(select).toBeInTheDocument();
+  });
+});
