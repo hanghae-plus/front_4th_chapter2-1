ac4b9ee2c8ede8667999043909eaea123ba08c4d
divopsor <divopsor@gmail.com>
2025-01-10 09:02:41 +0900

    refactor: calcCart 함수 UI 와 로직 분리

--- src/main.original.js
+++ src/main.original.js
@@ -29,6 +29,28 @@ function main() {
       { id: 'p5', name: '상품5', val: 25000, q: 10 },
     ],
   };
+  const { calcCart } = useCalcCart(({ totalAmt, discRate, prodList }) => {
+    sum.textContent = '총액: ' + Math.round(totalAmt) + '원';
+
+    if (discRate > 0) {
+      const span = document.createElement('span');
+      span.className = 'text-green-500 ml-2';
+      span.textContent = '(' + (discRate * 100).toFixed(1) + '% 할인 적용)';
+
+      sum.appendChild(span);
+    }
+
+    stockInfo.textContent = prodList
+      .filter((x) => x.q < 5)
+      .map(
+        (x) => `${x.name}: ${x.q > 0 ? `재고 부족 (${x.q}개 남음)` : '품절'}\n`,
+      )
+      .join('');
+
+    if (document.getElementById('loyalty-points') == null) {
+      sum.appendChild(getBonusPts({ totalAmt }));
+    }
+  });
 
   const app = createApp();
   const wrap = app.childNodes[0].childNodes[0];
@@ -94,80 +116,50 @@ function main() {
   }, Math.random() * 20000);
 }
 
-function calcCart({
-  sum,
-  cartDisp,
-  prodList,
-  totalAmt = 0,
-  itemCnt = 0,
-  stockInfo,
-} = {}) {
-  let subTot = 0;
+function useCalcCart(callback = () => {}) {
+  return {
+    calcCart: ({ cartDisp, prodList, totalAmt = 0, itemCnt = 0 } = {}) => {
+      let subTot = 0;
 
-  let curItem;
-  for (const cartItem of cartDisp.children) {
-    for (const product of prodList) {
-      if (product.id === cartItem.id) {
-        curItem = product;
-        break;
+      for (const cartItem of cartDisp.children) {
+        const curItem = prodList.find((x) => x.id === cartItem.id);
+        const q = parseInt(
+          cartItem.querySelector('span').textContent.split('x ')[1],
+        );
+
+        const itemTot = curItem.val * q;
+        const disc = q >= 10 ? DISCOUNT_RATE[curItem.id] : 0;
+        itemCnt += q;
+        subTot += itemTot;
+        totalAmt += itemTot * (1 - disc);
       }
-    }
-    const q = parseInt(
-      cartItem.querySelector('span').textContent.split('x ')[1],
-    );
 
-    const itemTot = curItem.val * q;
-    let disc = 0;
-    itemCnt += q;
-    subTot += itemTot;
+      let discRate = 0;
 
-    if (q >= 10) {
-      disc = DISCOUNT_RATE[curItem.id];
-    }
+      if (itemCnt >= 30) {
+        const bulkDisc = totalAmt * 0.25;
+        const itemDisc = subTot - totalAmt;
+        if (bulkDisc > itemDisc) {
+          totalAmt = subTot * (1 - 0.25);
+          discRate = 0.25;
+        } else {
+          discRate = (subTot - totalAmt) / subTot;
+        }
+      } else {
+        discRate = (subTot - totalAmt) / subTot;
+      }
 
-    totalAmt += itemTot * (1 - disc);
-  }
-
-  let discRate = 0;
-
-  if (itemCnt >= 30) {
-    const bulkDisc = totalAmt * 0.25;
-    const itemDisc = subTot - totalAmt;
-    if (bulkDisc > itemDisc) {
-      totalAmt = subTot * (1 - 0.25);
-      discRate = 0.25;
-    } else {
-      discRate = (subTot - totalAmt) / subTot;
-    }
-  } else {
-    discRate = (subTot - totalAmt) / subTot;
-  }
-
-  if (new Date().getDay() === 2) {
-    totalAmt *= 1 - 0.1;
-    discRate = Math.max(discRate, 0.1);
-  }
-
-  sum.textContent = '총액: ' + Math.round(totalAmt) + '원';
-
-  if (discRate > 0) {
-    const span = document.createElement('span');
-    span.className = 'text-green-500 ml-2';
-    span.textContent = '(' + (discRate * 100).toFixed(1) + '% 할인 적용)';
-
-    sum.appendChild(span);
-  }
-
-  stockInfo.textContent = prodList
-    .filter((x) => x.q < 5)
-    .map(
-      (x) => `${x.name}: ${x.q > 0 ? `재고 부족 (${x.q}개 남음)` : '품절'}\n`,
-    )
-    .join('');
-
-  if (document.getElementById('loyalty-points') == null) {
-    sum.appendChild(getBonusPts({ totalAmt }));
-  }
+      if (new Date().getDay() === 2) {
+        totalAmt *= 1 - 0.1;
+        discRate = Math.max(discRate, 0.1);
+      }
+      callback({
+        totalAmt,
+        discRate,
+        prodList,
+      });
+    },
+  };
 }
 
 main();
